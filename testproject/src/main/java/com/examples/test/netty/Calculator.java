8f73ea4e35f542dca8aeb14baf8384eef8c4ed9a
chenzhao <chenzhaoplus@163.com>
2021-07-05 19:03:30 +0800

    netty

--- testproject/src/main/java/com/examples/test/netty/Calculator.java
+++ testproject/src/main/java/com/examples/test/netty/Calculator.java
@@ -1,11 +1,14 @@
 package com.examples.test.netty;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+
 public enum Calculator {
-  Instance;
-  private final static ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
-  public Object cal(String expression) throws ScriptException{
-    return jse.eval(expression);
-  }
+    Instance;
+    private final static ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
+
+    public Object cal(String expression) throws ScriptException {
+        return jse.eval(expression);
+    }
 }
