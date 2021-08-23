8828c288788a481285ee4bd65e73044322afb500
chenzhao <chenzhaoplus@163.com>
2021-08-23 11:25:50 +0800

    动态bean

---
+++ testproject/src/main/java/com/examples/test/dynamicbean/User0.java
@@ -0,0 +1,19 @@
+package com.examples.test.dynamicbean;
+
+/**
+ * @author: cz
+ * @description:
+ */
+public class User0 {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
