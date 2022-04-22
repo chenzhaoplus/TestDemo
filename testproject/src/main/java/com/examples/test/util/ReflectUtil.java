cd4224e86a264190c4514a88085bbfc7be94a028
chenzhao <chenzhaoplus@163.com>
2022-04-22 15:48:31 +0800

    ReflectUtil

--- testproject/src/main/java/com/examples/test/util/ReflectUtil.java
+++ testproject/src/main/java/com/examples/test/util/ReflectUtil.java
@@ -16,6 +16,10 @@ public class ReflectUtil {
         dto.setCommunityId("123445");
         Object fieldValue = getFieldValue(dto, "communityId");
         System.out.println(fieldValue);
+
+        BasicFaceFeatureDTO instance = createInstance(BasicFaceFeatureDTO.class);
+        instance.setCommunityName("111");
+        System.out.println(instance);
     }
 
     public static <T> T getFieldValue(Object bean, String fieldName){
@@ -26,10 +30,22 @@ public class ReflectUtil {
             String name = field.getName();
             return (T) field.get(bean);
         } catch (NoSuchFieldException e) {
-            return null;
+            System.out.println("获取对象属性值失败，NoSuchFieldException：" + e);
         } catch (IllegalAccessException e) {
-            return null;
+            System.out.println("获取对象属性值失败，IllegalAccessException：" + e);
         }
+        return null;
+    }
+
+    public static <T> T createInstance(Class<T> clz) {
+        try {
+            return (T) clz.newInstance();
+        } catch (InstantiationException e) {
+            System.out.println("创建实例对象失败，InstantiationException：" + e);
+        } catch (IllegalAccessException e) {
+            System.out.println("创建实例对象失败，IllegalAccessException：" + e);
+        }
+        return null;
     }
 
 }
