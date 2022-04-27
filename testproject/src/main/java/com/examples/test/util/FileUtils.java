05086b9ec06519af8f7db441d065e880c8c7fdee
chenzhao <chenzhaoplus@163.com>
2022-04-27 15:53:21 +0800

    ImageUtil 图片压缩

--- testproject/src/main/java/com/examples/test/util/FileUtils.java
+++ testproject/src/main/java/com/examples/test/util/FileUtils.java
@@ -3,13 +3,18 @@ package com.examples.test.util;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ArrayUtil;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.io.FilenameUtils;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -23,6 +28,7 @@ public class FileUtils {
     private static final int ONE_MB = 1 * 1024 * 1024;
     // 1KB
     private static final int ONE_KB = 1 * 1024;
+    private static final Float MAX_PIC_LENGTH = 50.00f;
     // 文件结束标识
     private static final int EOF = -1;
     public static final String SUB_FILE_PREFIX = "sub-file-";
@@ -31,7 +37,10 @@ public class FileUtils {
         //String fileBase64ByUrl = getFileBase64ByUrl("http://test.smcaiot.com/group1/M00/40/16/rBAE1l_iowSAfPpyAACG9EMJ78s.d1fe6f");
         //System.out.println(fileBase64ByUrl);
 
-        testSliceMergeFile();
+        //testSliceMergeFile();
+
+        //testImageCompression();
+
     }
 
     private static void testSliceMergeFile() throws IOException {
@@ -119,5 +128,81 @@ public class FileUtils {
             }
         }
     }
+
+    //public static void testImageCompression() throws IOException {
+    //    String oldImg = "D:\\zip\\pic\\1.jpg";
+    //    String newImg = "D:\\zip\\pic\\2.jpg";
+    //    Float qty = 0.1f;
+    //    imageCompression(oldImg, newImg);
+    //}
+    //
+    //public static void imageCompression(String oldImg, String newImg) throws IOException {
+    //    imageCompression(oldImg, newImg, null);
+    //}
+    //
+    //public static void imageCompression(String oldImg, String newImg, Float quality) throws IOException {
+    //    imageCompression(oldImg, newImg, null, null, quality);
+    //}
+    //
+    ///**
+    // * @param imgsrc  原图片路径
+    // * @param imgdist 压缩后新文件路径
+    // * @param width   宽
+    // * @param height  高
+    // * @param quality 图片质量 (0 - 1.0f)浮点型
+    // * @author kcy
+    // * @version 2018-11-8
+    // */
+    //public static void imageCompression(String oldImg, String newImg, Integer width, Integer height, Float quality) throws IOException {
+    //    //Thumbnails.of() <- 这个方法虽然可以直接使用原图路径读取图片,但是有很大几率会使压缩后的图片颜色失真。也建议不要用ImageIO.read()这种方法读取图片，灰常容易失真!
+    //    //使用Toolkit.getDefaultToolkit().getImage()读取图片，防止图片颜色失真。
+    //    Image image = Toolkit.getDefaultToolkit().getImage(oldImg);
+    //    //Image转换BufferedImage
+    //    BufferedImage bufferedImage = toBufferedImage(image);
+    //    quality = Optional.ofNullable(quality).orElse(getPicQualityByRatio(oldImg));
+    //    width = Optional.ofNullable(width).orElse(bufferedImage.getWidth() / 2);
+    //    height = Optional.ofNullable(height).orElse(bufferedImage.getHeight() / 2);
+    //    //图片压缩
+    //    Thumbnails.of(bufferedImage).size(width, height).outputFormat("jpg").keepAspectRatio(true).outputQuality(quality).toFile(newImg);
+    //}
+    //
+    //private static Float getPicQualityByRatio(String filePath){
+    //    long length = getFileLength(filePath) / ONE_KB;
+    //    return Float.valueOf(MAX_PIC_LENGTH / length);
+    //}
+    //
+    //private static long getFileLength(String filePath){
+    //    File file = new File(filePath);
+    //    return file.length();
+    //}
+    //
+    //public static BufferedImage toBufferedImage(Image image) {
+    //    if (image instanceof BufferedImage) {
+    //        return (BufferedImage) image;
+    //    }
+    //    // This code ensures that all the pixels in the image are loaded
+    //    image = new ImageIcon(image).getImage();
+    //    BufferedImage bimage = null;
+    //    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    //    try {
+    //        int transparency = Transparency.OPAQUE;
+    //        GraphicsDevice gs = ge.getDefaultScreenDevice();
+    //        GraphicsConfiguration gc = gs.getDefaultConfiguration();
+    //        bimage = gc.createCompatibleImage(image.getWidth(null), image.getHeight(null), transparency);
+    //    } catch (HeadlessException e) {
+    //        // The system does not have a screen
+    //    }
+    //    if (bimage == null) {
+    //        // Create a buffered image using the default color model
+    //        int type = BufferedImage.TYPE_INT_RGB;
+    //        bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+    //    }
+    //    // Copy image to buffered image
+    //    Graphics g = bimage.createGraphics();
+    //    // Paint the image onto the buffered image
+    //    g.drawImage(image, 0, 0, null);
+    //    g.dispose();
+    //    return bimage;
+    //}
 
 }
---
+++ testproject/src/main/java/com/examples/test/util/ImageUtil.java
@@ -0,0 +1,117 @@
+package com.examples.test.util;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageOutputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Arrays;
+
+public class ImageUtil {
+
+    public static void main(String[] args) throws IOException {
+
+        String srcFile = "D:\\zip\\pic\\1.jpg";
+        String zipFile = "D:\\zip\\pic\\2.jpg";
+        compressImg(srcFile, 50 * 1024, zipFile);
+
+    }
+
+    public static void compressImg(String srcFile, long maxSize, String zipFile) throws IOException {
+        compressImg(new File(srcFile), maxSize, new File(zipFile));
+    }
+
+    /**
+     * 将图片压缩到指定大小以内
+     *
+     * @param srcImgData 源图片数据
+     * @param maxSize    目的图片大小
+     * @return
+     * @author CY
+     * @date 2020年11月18日
+     */
+    public static void compressImg(File imageFile, long maxSize, File zipFile) throws IOException {
+        long timeStart = System.currentTimeMillis();
+        zipFile.delete();
+        byte[] data = getByteByPic(imageFile);
+        byte[] imgData = Arrays.copyOf(data, data.length);
+        do {
+            try {
+                imgData = compress(imgData, 0.8);
+            } catch (IOException e) {
+                throw new IllegalStateException("压缩图片过程中出错,请及时联系管理员!", e);
+            }
+        } while (imgData.length > maxSize);
+        byteToImage(imgData, zipFile);
+        long timeEnd = System.currentTimeMillis();
+        System.out.println("耗时：" + (timeEnd - timeStart));
+    }
+
+    /**
+     * 获取图片文件字节
+     *
+     * @param imageFile
+     * @return
+     * @throws IOException
+     * @author CY
+     * @date 2020年11月18日
+     */
+    public static byte[] getByteByPic(File imageFile) throws IOException {
+        InputStream inStream = new FileInputStream(imageFile);
+        BufferedInputStream bis = new BufferedInputStream(inStream);
+        BufferedImage bm = ImageIO.read(bis);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        String imageUrl = imageFile.getAbsolutePath();
+        String type = imageUrl.substring(imageUrl.length() - 3);
+        ImageIO.write(bm, type, bos);
+        bos.flush();
+        byte[] data = bos.toByteArray();
+        return data;
+    }
+
+    /**
+     * 按照宽高比例压缩
+     *
+     * @param srcImgData 待压缩图片输入流
+     * @param scale      压缩刻度
+     * @return
+     * @throws IOException
+     * @author CY
+     * @date 2020年11月18日
+     */
+    public static byte[] compress(byte[] srcImgData, double scale) throws IOException {
+        BufferedImage bi = ImageIO.read(new ByteArrayInputStream(srcImgData));
+        int width = (int) (bi.getWidth() * scale); // 源图宽度
+        int height = (int) (bi.getHeight() * scale); // 源图高度
+        Image image = bi.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage tag = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = tag.getGraphics();
+        g.setColor(Color.RED);
+        g.drawImage(image, 0, 0, null); // 绘制处理后的图
+        g.dispose();
+        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+        ImageIO.write(tag, "JPEG", bOut);
+        return bOut.toByteArray();
+    }
+
+    /**
+     * byte数组转图片
+     *
+     * @param data
+     * @param path
+     * @author CY
+     * @date 2020年11月18日
+     */
+    public static void byteToImage(byte[] data, File zipFile) {
+        if (data.length < 3)
+            return;
+        try {
+            FileImageOutputStream imageOutput = new FileImageOutputStream(zipFile);
+            imageOutput.write(data, 0, data.length);
+            imageOutput.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
